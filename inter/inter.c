143eb29979a55b1f6d81aa76ce81967fed8174da
khuzha <mr.khuzha@gmail.com>
2021-02-27 13:42:34 +0300

    looks like ready

--- inter/inter.c
+++ inter/inter.c
@@ -7,6 +7,7 @@ int	includes(char *str, char c)
 	{
 		if (*str == c)
 			return (1);
+		str++;
 	}
 	return (0);
 }
@@ -16,7 +17,7 @@ int	includes_lim(char *str, char c, size_t pos)
 	size_t	i;
 
 	i = 0;
-	while (str && str[i] && i <= pos)
+	while (str && str[i] && i < pos)
 	{
 		if (str[i] == c)
 			return (1);
@@ -34,9 +35,12 @@ int	main (int argc, char **argv)
 	if (argc != 3)
 		return ((int) write(1, "\n", 1));
 	if (!argv[1] || !argv[2])
-		return ;
+		return (-1);
 	while (argv[1][i] && argv[2][i])
 	{
-
+		if (includes(argv[2], argv[1][i]) && !includes_lim(argv[1], argv[1][i], i))
+			write(1, &argv[1][i], 1);
+		i++;
 	}
+	write(1, "\n", 1);
 }
