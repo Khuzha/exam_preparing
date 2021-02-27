64ce62d0c5144c8debba56dbfde911f56e17eb0f
khuzha <mr.khuzha@gmail.com>
2021-02-27 15:36:20 +0300

    fix union

---
+++ union.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int	includes(char *str, char c)
+{
+	while (str && *str)
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
+	char	arr[256];
+	size_t	i;
+	size_t	j;
+
+	if (argc != 3)
+		return (write(1, "\n", 1));
+	i = 0;
+	j = 0;
+	while (i < 256)
+	{
+		arr[i] = 0;
+		i++;
+	}
+	i = 1;
+	while (i <= 2)
+	{
+		while (argv[i][j])
+		{
+			// printf("arr = %s, curchar = %c, includes = %d\n", arr, argv[i][j], includes(arr, argv[i][j]));
+			if (!includes(arr, argv[i][j]))
+			{
+				arr[ft_strlen(arr)] = argv[i][j];
+				// printf("pos = %d\n", (int)(ft_strlen(arr)));
+				write(1, &argv[i][j], 1);
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	write (1, "\n", 1);
+}
